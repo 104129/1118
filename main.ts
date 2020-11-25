e8aef74cb163f69c4bba757656c4af10a1371556
104129 <70998466+104129@users.noreply.github.com>
2020-11-25 14:55:02 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,47 @@
 input.onGesture(Gesture.LogoUp, function () {
-    if (頁面 == 1) {
-        radio.sendNumber(1)
+	
+})
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        bitbot.go(BBDirection.Reverse, 60)
+    }
+    if (receivedNumber == 2) {
+        bitbot.go(BBDirection.Forward, 60)
+    }
+    if (receivedNumber == 3) {
+        bitbot.rotate(BBRobotDirection.Left, 60)
+    }
+    if (receivedNumber == 4) {
+        bitbot.rotate(BBRobotDirection.Right, 60)
+    }
+    if (receivedNumber == 5) {
+        bitbot.stop(BBStopMode.Brake)
+    }
+    if (receivedNumber == 6) {
+        for (let index = 0; index < 2; index++) {
+            bitbot.buzz(true)
+            basic.pause(100)
+            bitbot.buzz(false)
+            basic.pause(100)
+        }
+    }
+    if (receivedNumber == 7) {
+        距離循線()
+    }
+    if (receivedNumber == 8) {
+        距離 = bitbot.sonar(BBPingUnit.Centimeters)
+        basic.showNumber(距離)
+        radio.sendValue("Z", 距離)
+    }
+    if (receivedNumber == 9) {
+        bitbot.stop(BBStopMode.Brake)
     }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    if (頁面 == 1) {
-        radio.sendNumber(3)
-    }
+	
 })
 input.onButtonPressed(Button.A, function () {
-    if (頁面 == 1) {
-        radio.sendNumber(5)
-    }
-    if (頁面 == 2) {
-        radio.sendNumber(7)
-    }
+	
 })
 function 循線 () {
     // 白線上
@@ -55,46 +82,36 @@ function 循線 () {
     }
 }
 input.onGesture(Gesture.Shake, function () {
-    if (頁面 == 2) {
-        radio.sendNumber(6)
-    }
+	
 })
 function 距離循線 () {
-    if (bitbot.sonar(BBPingUnit.Centimeters) <= 10) {
-        bitbot.stop(BBStopMode.Brake)
-    } else {
+    while (bitbot.sonar(BBPingUnit.Centimeters) > 10) {
         循線()
     }
+    bitbot.stop(BBStopMode.Brake)
 }
 input.onGesture(Gesture.LogoDown, function () {
-    if (頁面 == 1) {
-        radio.sendNumber(2)
-    }
+	
 })
 input.onButtonPressed(Button.AB, function () {
-    if (頁面 < 2) {
-        頁面 += 1
-        basic.showNumber(頁面)
-    } else {
-        頁面 = 1
-        basic.showNumber(頁面)
-    }
+	
 })
 input.onButtonPressed(Button.B, function () {
-    if (頁面 == 2) {
-        radio.sendNumber(8)
-    }
+	
 })
 input.onGesture(Gesture.TiltRight, function () {
-    if (頁面 == 1) {
-        radio.sendNumber(4)
+	
+})
+radio.onReceivedValue(function (name, value) {
+    if ("Z" == name) {
+        basic.showNumber(value)
     }
 })
 let 結束時間 = 0
 let 開始時間 = 0
-let 頁面 = 0
+let 距離 = 0
 radio.setGroup(2)
-頁面 = 1
+let 頁面 = 1
 basic.showNumber(頁面)
 basic.forever(function () {
 	
